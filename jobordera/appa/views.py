b3a438cea379878d48e0c5f5b2bf885efc78b7cc
liekiewaa <33012450+laikaiwaa@users.noreply.github.com>
2022-03-18 17:16:40 +0800

    Update views.py

--- jobordera/appa/views.py
+++ jobordera/appa/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from appa.model import userinfo, loginhistory
+from appa.model import userinfo, loginhistory, checklisttemplte, formfilelist
 from django.db import connection, connections, models
 import logging
 import datetime
@@ -84,65 +84,107 @@ def admin(request):
     username = request.POST.get('username')
     usercode = request.POST.get('usercode')
     usertype = request.POST.get('usertype')
+    try:
+        btime = request.POST.get('btime')
+        etime = request.POST.get('etime')
+    except:
+        pass
     if (status is None) | (request.session['userkind'] != 'adminer'):
         return HttpResponseRedirect('../')
     else:
         if request.method == "GET":
+            request.session['htmlname'] = "admin.html"
             return render(request, 'admin.html', {"list": [], "refrash": ""})
         elif request.method == "POST":
             data = None
-            if request.POST.get("check"):
-                data = admincheck(username)
-                # return render(request, 'admin.html', {"list": data, "refrash": username})
-            elif request.POST.get("add"):
-                data = adminadd(username, usercode, usertype)
-                os.mkdir(filepath + "./" + username)
-            elif request.POST.get("all"):
-                data = adminall()
-            elif request.POST.get("usehistory"):
-                #return render(request, 'usehistory.html')
-                return HttpResponseRedirect('../usehistory')
-            elif request.POST.get("checklisttemplate"):
-                #return render(request, 'checklisttemplate.html', {"list": data, "refrash": username})
-                return HttpResponseRedirect('/checklisttemplate')
-            return render(request, 'admin.html', {"list": data, "refrash": username})
-
-
-def usehistory(request):
-    status = request.COOKIES.get("name")
-    btime = request.POST.get('btime')
-    etime = request.POST.get('etime')
-    if status is None:
-        return HttpResponseRedirect('../')
-    else:
-        if request.method == "GET":
-            return render(request, 'usehistory.html', {"list": [], "btime": "", "etime": ""})
-        elif request.method == "POST":
-            if request.POST.get("serch"):
-                data = serch(btime, etime)
-                return render(request, 'usehistory.html', {"list": data, "btime": btime, "etime": etime})
+            if (request.POST.get("checklisttemplate") is not None) | (
+                    request.session['htmlname'] == "checklisttemplate.html"):
+                request.session['htmlname'] = "checklisttemplate.html"
+                retrundata = checklisttemplate(request)
+                return render(request, 'checklisttemplate.html', retrundata)
+            elif (request.POST.get("usehistory") is not None) | (
+                    request.session['htmlname'] == "usehistory.html"):
+                request.session['htmlname'] = "usehistory.html"
+                retrundata = usehistory(request)
+                return render(request, 'usehistory.html', retrundata)
+            else:
+                request.session['htmlname'] = "admin.html"
+                if request.POST.get("check"):
+                    data = admincheck(username)
+                elif request.POST.get("add"):
+                    data = adminadd(username, usercode, usertype)
+                    os.mkdir(filepath + "./" + username)
+                elif request.POST.get("all"):
+                    data = adminall()
+                return render(request, 'admin.html', {"list": data, "refrash": username})
 
 
 def user(request):
     status = request.COOKIES.get("name")
     usrname = request.session['username']
-    testloop = [i for i in range(3)]
+    templteselected = request.POST.get('templteselect')
+    try:
+        btime = request.POST.get('btime')
+        etime = request.POST.get('etime')
+    except:
+        pass
+    if templteselected is None:
+        templteselected = ""
+    formfilekind = ""
+    formlist = [""]
 
     if (status is None) | (request.session['userkind'] != 'user'):
         return HttpResponseRedirect('../')
     else:
-        if request.method == "GET":
+        cursor = connection.cursor()
+        formnames = cursor.execute("select templtename from appa_checklisttemplte").fetchall()
+        formmessage = cursor.execute(
+            "select formlist,templtekind from appa_checklisttemplte where templtename='" + templteselected + "'").fetchall()
+        if len(formmessage) >= 1:
+            formlist = formmessage[0][0].split(";")[:-1]
+            formfilekind = formmessage[0][1]
+            cantainlen = len(formlist)
 
-            return render(request, 'user.html',{"testloop":testloop})
+        userdata = {"testloop": [["", 0], ["", 1]], "namelist": formnames, "result": [{"name": "", "size": 0}],
+                    "selectnow": ""}
+
+        if request.method == "GET":
+            request.session['htmlname'] = "user.html"
+            return render(request, 'user.html', userdata)
         elif request.method == "POST":
-            if request.POST.get("usehistory"):
-                return HttpResponseRedirect('../usehistory')
-            elif request.POST.get("updata"):
-                checkboxlist = [[testloop[i], request.POST.get("checkbox" + str(i))] for i in range(3)]
-                print(checkboxlist)
-                files = request.FILES.getlist('chosefiles')
-                upformdata(files, usrname)
-                return render(request, 'user.html', {"result": files})
+            formfileid = 1
+            userdata2 = {"formnamelist": ["", ""], "formcantans": ["", ""], "selectednow": 0}
+            if (request.POST.get("usehistory") is not None) | (
+                    request.session['htmlname'] == "usehistory.html"):
+                request.session['htmlname'] = "usehistory.html"
+                retrundata = usehistory(request)
+                return render(request, 'usehistory.html', retrundata)
+            elif (request.POST.get("myform") is not None) | (
+                    request.session['htmlname'] == "myform.html"):
+                request.session['htmlname'] = "myform.html"
+                if request.POST.get("myform"):
+                    userdata2['formnamelist'] = myform(request, usrname, formfileid)
+                    request.session['formnamelist'] = userdata2['formnamelist']
+                else:
+                    userdata2['selectednow'] = int(request.POST.get("templteselect"))
+                    userdata2['formcantans'] = myform(request, usrname, str(userdata2['selectednow']))[0]
+                    userdata2['formnamelist'] = request.session['formnamelist']
+                    print("还是生活时尚生活2", userdata2['formcantans'][0][1:-1])
+                return render(request, 'myform.html', userdata2)
+            else:
+                request.session['htmlname'] = "user.html"
+                if request.POST.get("updata"):
+                    formcantain = [[formlist[i] , request.POST.get("checkbox" + str(i))] for i in range(cantainlen)]
+                    files = request.FILES.getlist('chosefiles')
+                    upformdata(files, usrname, formfilekind, templteselected, formcantain)
+                    operationmark(usrname, "updata")
+                    return render(request, 'finish.html')
+                elif request.POST.get("templteselect"):
+                    userdata['testloop'] = [[formlist[i], i] for i in range(cantainlen)]
+                    userdata['namelist'] = formnames
+                    userdata['result'] = [{"name": "", "size": 0}]
+                    userdata['selectnow'] = templteselected
+                    return render(request, 'user.html', userdata)
 
 
 def set_cookietest(HttpResponseRedirect):
@@ -150,27 +192,110 @@ def set_cookietest(HttpResponseRedirect):
     return co
 
 
-def upformdata(form, usrname):
+def usehistory(request):
+    btime = request.POST.get('btime')
+    etime = request.POST.get('etime')
+    retrundata = {"list": [], "btime": "", "etime": ""}
+    if request.POST.get("serch"):
+        data = serch(btime, etime)
+        retrundata['list'] = data
+        retrundata['btime'] = btime
+        retrundata['etime'] = etime
+    return retrundata
+
+
+def upformdata(form, usrname, formfilekind, formtempltename, formcantain):
+    # 读取基础信息
+    cursor = connection.cursor()
+    k = cursor.execute("select max(formfileid) from appa_formfilelist where uploader='" + usrname + "'").fetchall()[0][
+        0]
+
+    # 读取文档
+    fileaddress = list()
     for eachfile in form:
         timemark = datetime.datetime.strftime(datetime.datetime.now(), "%Y_%m_%d_%H_%M_%S")
-        with open(filepath + "\\" + usrname + "\\" + str.replace(timemark, " :", "_") + "_" + eachfile.name, 'wb') as f:
+        name = usrname + "//" + str(k + 1) + "_" + formtempltename + "_" + str.replace(timemark, " :",
+                                                                                       "_") + "_" + eachfile.name
+        with open(filepath + "//" + name, 'wb') as f:
             for i in eachfile.chunks():
                 f.write(i)
-    return None
+        fileaddress.append(name)
+
+    # 信息写库
+    formtime = datetime.datetime.strftime(datetime.datetime.now(), "%Y_%m_%d_%H_%M_%S")
+    if k is None:
+        k = 0
+    formfilelist.objects.create(formfileid=k + 1, formfilename=formtempltename, formfilekind=formfilekind,
+                                createtime=formtime, formcantain=formcantain, fileaddress=fileaddress, uploader=usrname)
+    return fileaddress
+
 
 def checklisttemplate(request):
-    status = request.COOKIES.get("name")
+    retrundata = {"httempltename": "", 'resultlist': [["", ""]], 'file': "", "othinfo": ["", "", ""],
+                  "namelist": [""], "selectnow": ""}
     cursor = connection.cursor()
+    request.session['templtenamelist'] = cursor.execute(
+        "select templtename from appa_checklisttemplte").fetchall()
+    # 返回值
+    retrundata['namelist'] = request.session['templtenamelist']
+    retrundata['selectnow'] = request.POST.get("templtename")
+
+    if request.POST.get("searchtemplte"):
+        templtename = request.POST.get("templteselect")
+        tform = checklisttemplte.objects.filter(templtename=templtename).values()[0]
+        # 返回值
+        retrundata['httempltename'] = tform['templtename']
+        retrundata['resultlist'] = strtolist(tform['formlist'])
+        retrundata['othinfo'] = [tform['templteid'], tform['templtekind'], tform['createtime']]
+        retrundata['namelist'] = request.session['templtenamelist']
+        retrundata['selectnow'] = templtename
+        retrundata['file'] = tform['fileaddress']
+    elif request.POST.get("addtemplte"):
+        retrundata['namelist'] = addtemplate(templtename=request.POST.get("templtename"),
+                                             templtekind=request.POST.get("templtekind"),
+                                             formlist="testa, 2;testb,3;", fileaddress="noaddres")
+    elif request.POST.get("deletetemplte"):
+        deletetemplate(request.POST.get("templteselect"))
+        cursor = connection.cursor()
+        request.session['templtenamelist'] = cursor.execute(
+            "select templtename from appa_checklisttemplte").fetchall()
+        # 返回值
+        retrundata['namelist'] = request.session['templtenamelist']
+    return retrundata
+
+
+def addtemplate(templtename, templtekind, formlist, fileaddress):
+    cursor = connection.cursor()
+    k = cursor.execute("select max(templteid) from appa_checklisttemplte").fetchall()[0][0]
+    checklisttemplte.objects.create(templteid=k + 1, templtename=templtename, templtekind=templtekind,
+                                    formlist=formlist,
+                                    fileaddress=fileaddress)
     templtenamelist = cursor.execute("select templtename from appa_checklisttemplte").fetchall()
-    if (status is None) :
-        return HttpResponseRedirect('../')
-    else:
-        if request.method == "GET":
-            return render(request, 'checklisttemplate.html', {"list": templtenamelist})
-        elif request.method == "POST":
-            data = None
-            if request.POST.get("addtemplte"):
-                data = admincheck()
-                return HttpResponseRedirect('/checklisttemplate')
-            return render(request, 'admin.html', {"list": data})
+    return templtenamelist
+
+
+def deletetemplate(templtename):
+    cursor = connection.cursor()
+    k = cursor.execute("delete  from appa_checklisttemplte where templtename='" + templtename + "'").fetchall()
     return None
+
+
+def strtolist(str):
+    la = list()
+    al = str.split(";")
+    for i in al:
+        la.append(list(i.split(',')))
+    return la[:-1]
+def strtolistfrombase(str):
+    la = list()
+
+def myform(request, name, formfileid):
+    cursor = connection.cursor()
+    if request.POST.get("myform"):
+        formnamelist = cursor.execute(
+            "select formfileid,formfilename from appa_formfilelist where uploader='" + name + "'").fetchall()
+        return formnamelist
+    else:
+        formmessage = cursor.execute(
+            "select formcantain from appa_formfilelist where formfileid='" + formfileid + "' and uploader='" + name + "'").fetchall()
+        return formmessage
