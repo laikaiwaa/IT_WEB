2b9182398d7b6497961166b50c6083bea575a7e1
liekiewaa <laikaiwaa@gmail.com>
2022-04-10 22:57:07 +0800

    20220410

---
+++ jobordera/appa/model.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class userinfo(models.Model):
+    userid = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=30, unique=True, null=False)
+    password = models.CharField(max_length=50)
+    type = models.CharField(max_length=5)
+
+class loginhistory(models.Model):
+    userid = models.IntegerField()
+    username = models.CharField(max_length=30)
+    time = models.DateTimeField(auto_now=True,primary_key=True)
+    actionkind = models.CharField(max_length=30)
+
+class checklisttemplte(models.Model):
+    templteid = models.IntegerField(primary_key=True)
+    templtename = models.CharField(max_length=100)
+    templtekind = models.CharField(max_length=30)
+    createtime = models.DateTimeField(auto_now=True)
+    formlist = models.CharField(max_length=1000)
+    fileaddress = models.CharField(max_length=100)
+
+class formfilelist(models.Model):
+    formfileid = models.IntegerField()
+    formfilename = models.CharField(max_length=100)
+    formfilekind = models.CharField(max_length=30)
+    createtime = models.DateTimeField(auto_now=True,primary_key=True)
+    formcantain = models.CharField(max_length=1000)
+    fileaddress = models.CharField(max_length=100)
+    uploader = models.CharField(max_length=30)
+    filestatus = models.CharField(max_length=10)
+
---
+++ jobordera/appa/urls.py
@@ -0,0 +1,32 @@
+"""django2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import random
+from django.contrib import admin
+from django.urls import path
+import appa.views as views
+from django.conf.urls.static import static
+from django.conf  import settings
+
+adminr=random.randint(0,9)
+urlpatterns = [
+    path('', views.login),
+    path('admin/', views.admin),
+    path('usehistory/', views.usehistory),
+    path('user/',views.user),
+    path('xuser/',views.xuser),
+    path('liekiewaa/',views.jb),
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
