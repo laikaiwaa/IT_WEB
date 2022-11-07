6653718f781843788397f2f7ccc6812398934824
liekiewaa <33012450+laikaiwaa@users.noreply.github.com>
2022-11-07 15:15:20 +0800

    Update views.py

--- streaming/testapp/views.py
+++ streaming/testapp/views.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import time
+import base64
 
 from django.http import StreamingHttpResponse, FileResponse, JsonResponse
 from django.http import HttpResponseRedirect
@@ -24,32 +25,77 @@ import numpy as np
 
 
 def makepicture():
-    r=0
+    r = 0
     while (True):
-        r=r+0.01
+        r = r + 0.01
         time.sleep(0)
-        #im = ImageGrab.grab()
-        #frame = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-        frame=cv2.imread("/root/liekiewaa/web/imagefiles/static/k.png")
-        sp=frame.shape	
+        # im = ImageGrab.grab()
+        # frame = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+        path="E:/manualmaintenance/imagefiles/"
+        try:
+            dirs=os.listdir(path)
+            dirs.sort()
+            for i in dirs:
+                if i.__contains__(".jpg"):
+                    frame = cv2.imread(path+i)
+                    _, g = cv2.imencode('.jpg', frame)
+                    yield (b'--frame\r\n'
+                           b'Content-Type:image/jpeg\r\n\r\n' + g.tobytes() + b'\r\n\r\n')
+                    os.remove(path+i)
+        except:
+            time.sleep(5)
+        # sp = frame.shape
+        #
+        # if (r <= 0.001) | (r > 1):
+        #     r = 0.01
+        # frame = frame[:int(sp[0] * r), :int(sp[1] * r), :]
 
-        if (r<=0.001) | (r>1):
-               r=0.01
-        frame=frame[:int(sp[0]*r),:int(sp[1]*r),:]
+        # _, g = cv2.imencode('.jpg', frame)
 
-        _, g = cv2.imencode('.jpg', frame)
 
-        yield (b'--frame\r\n'
-               b'Content-Type:image/jpeg\r\n\r\n' + g.tobytes() + b'\r\n\r\n')
 
 
 def sendvideo(request):
     return StreamingHttpResponse(makepicture(), content_type="multipart/x-mixed-replace;boundary=frame")
 
-
+# 寫成視頻文件
 def watchcontrol(request):
     if request.method == "GET":
         return render(request, 'watch.html')
-    #if request.method =="POST":
-    #    if request.POST.get("close"):
-   #         return render(request, 'watch.html')
+    if request.method == "POST":
+        if request.POST.get("sendmark"):
+            form = request.POST.get("stream").split(",")[1]
+            image2 = base64.b64decode(form)
+            # 写入档案
+            tempfilepath = "E:/manualmaintenance/imagefiles/img_"+str(datetime.datetime.now().time()).replace(":","_")+".jpg"
+            tempfilepathv = "E:/manualmaintenance/imagefiles/img.avi"
+            # try:
+            #     w=cv2.VideoCapture(tempfilepathv)
+            # except:
+            #     w = cv2.VideoWriter(tempfilepathv, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 60,
+            #                              (500, 500))
+            try:
+                with open(tempfilepath, 'wb') as f:
+                    f.write(image2)
+                    f.close
+                # img=cv2.imread(tempfilepath)
+                # w.write(img)
+            except Exception as dd:
+                print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", dd)
+            return render(request, 'watch.html')
+# def watchcontrol(request):
+#     if request.method == "GET":
+#         return render(request, 'watch.html')
+#     if request.method == "POST":
+#         if request.POST.get("sendmark"):
+#             form = request.POST.get("stream").split(",")[1]
+#             image2 = base64.b64decode(form)
+#             # 写入档案
+#             tempfilepath = "E:/manualmaintenance/imagefiles/img.jpg"
+#             try:
+#                 with open(tempfilepath, 'wb') as f:
+#                     f.write(image2)
+#                     f.close
+#             except Exception as dd:
+#                 print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", dd)
+#             return render(request, 'watch.html')
